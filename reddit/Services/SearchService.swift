56bcfdddba1cf76f3ce840a91c67a67ef5c5e66d
Allen Huang <allen.huang@grindr.com>
2018-02-24 08:41:02 -0800

    hitting api and getting results

---
+++ reddit/Services/SearchService.swift
@@ -0,0 +1,38 @@
+//
+//  SearchService.swift
+//  reddit
+//
+//  Created by Allen Huang on 2/24/18.
+//  Copyright © 2018 Allen Huang. All rights reserved.
+//
+
+import Foundation
+
+class SearchService {
+    static let shared = SearchService()
+
+    private let host = "reddit.com"
+    private let scheme = "https"
+
+    func getTopPosts(completion: @escaping ([Post]?, Error?) -> ()) {
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = host
+        components.path = "/top.json"
+
+        URLSession.shared.dataTask(with: components.url!) { data, response, error in
+            if let data = data {
+//                                let jsonRepresentation = try! JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments)
+//                                print(jsonRepresentation)
+                do {
+                    let decoder = JSONDecoder()
+                    let results = try decoder.decode(TopResults.self, from: data)
+                    completion(results.data.children, nil)
+
+                } catch let error as NSError {
+                    completion(nil, error)
+                }
+            }
+            }.resume()
+    }
+}
--- reddit/ViewController.swift
+++ reddit/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let tableView = UITableView(frame: .zero, style: .plain)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        SearchService.shared.getTopPosts { (posts, error) in
+            print("results:\(posts)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        return cell!
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 }
 
